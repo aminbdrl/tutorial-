cf15c8bad16b3cac2fb1a59e40c003931bc3242f
aminbdrl <maminuddeenh@gmail.com>
2025-10-29 18:31:22 +0800

    Replace plt.show() with st.pyplot() for Streamlit

--- tsp.py
+++ tsp.py
@@ -61,7 +61,7 @@ for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
             ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
 
 fig.set_size_inches(16, 12)
-plt.show()
+st.pyplot(fig)
 
 #population
 def initial_population(cities_list, n_population = 250):
@@ -318,4 +318,4 @@ for i, txt in enumerate(shortest_path):
 
 fig.set_size_inches(16, 12)
 # plt.grid(color='k', linestyle='dotted')
-plt.show()
+st.pyplot(fig)
